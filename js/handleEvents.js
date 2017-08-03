1b4ef8a650aab4017a0baec942821adff1d6e7d1
Héctor Campos <hectoraimx@gmail.com>
2017-08-03 10:55:55 +0200

    Cleaned file
    Not used component deleted.

--- js/handleEvents.js
+++ js/handleEvents.js
@@ -1,25 +1,3 @@
-AFRAME.registerComponent('handle-events', {
-   init: function () {
-     var el = this.el;  //
-     el.addEventListener('mouseenter', function () {
-       el.setAttribute('color', 'red');
-       console.log("mouseenter");
-     });
-     el.addEventListener('mouseleave', function () {
-       el.setAttribute('color', 'grey');
-         console.log("mouseleave");
-     });
-     el.addEventListener('click', function () {
-       el.setAttribute('position', {x: numAleatorio(1,5), y: numAleatorio(1,3), z: numAleatorio(1,3)});
-         console.log("click");
-     });
-
-     function numAleatorio(min, max) {
-       return Math.round(Math.random() * (max - min) + min);
-       }
-    }
- });
-
 AFRAME.registerComponent('attack-button', {
   schema: {
     targetEntity: {type: 'string', default: '#Ghost01'}
